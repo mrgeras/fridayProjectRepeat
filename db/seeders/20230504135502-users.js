10f19a90d4e4e2962cb0c978ac4babb0a1d62fcd
Alexxx2173 <Alexxx.Aksenov@gmail.com>
2023-05-04 17:18:54 +0300

    db seed create

---
+++ db/seeders/20230504135502-users.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          login: 'Фёдор',
+          password: await bcrypt.hash('123', 10),
+          count: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          login: 'Петя',
+          password: await bcrypt.hash('1234', 10),
+          count: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
