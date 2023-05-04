954e903b357729716e87860dd9950aec30136ac5
Ilia <gerasimbek3@gmail.com>
2023-05-04 19:47:08 +0300

    v1

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,7 +10,7 @@ function Layout({ children, title }) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="stylesheet" href="/style.css" />
+        <link rel="stylesheet" href="/css/style.css" />
         <title>{title} </title>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -2,31 +2,49 @@ const React = require('react');
 const Layout = require('./Layout');
 const Themes = require('./Themes');
 
-module.exports = function MainPage({ themes,title }) {
+module.exports = function MainPage({ themes, title }) {
   return (
     <Layout title={title}>
-      <h1>W E L C O M E!</h1> <br/>
-      <h4>Моя игра, моя игра
-Она мне принадлежит и таким же как и я,
-Моя игра, моя игра
-Она мне принадлежит и таким же как и я,
-
-Моя игра, моя игра,
-Здесь правила одни, и цель одна,
-
-Моя игра, моя игра
-Она мне принадлежит и таким же как и я
-Моя игра, моя игра
-Здесь правила одни, и цель одна</h4> <br/>
-
-<h3>Чтобы сыграть в эту Раз*ёмную игру, ЗАРЕГИСТРИРУЙСЯ (АВТОРИЗУЙСЯ)</h3>
-<br/>
-      <div className="mainBlock">
-       
-        {themes.map((theme) => (
-          //  Передаем темы
-          <Themes key={theme.id} theme={theme} showButtons={false} />
-        ))}
+      <div className="mainContainer">
+        <div className="welcome">
+          <h1>W E L C O M E!</h1>
+        </div>
+        <br />
+        <div className="myPlay">
+          <p>
+            Моя игра, моя игра
+            <br />
+            Она мне принадлежит и таким же как и я,
+            <br />
+            Моя игра, моя игра
+            <br />
+            Она мне принадлежит и таким же как и я,
+            <br />
+            <br />
+            Моя игра, моя игра,
+            <br />
+            Здесь правила одни, и цель одна,
+            <br />
+            <br />
+            Моя игра, моя игра
+            <br />
+            Она мне принадлежит и таким же как и я<br />
+            Моя игра, моя игра
+            <br />
+            Здесь правила одни, и цель одна
+          </p>
+        </div>
+        <br />
+        <h3>
+          Чтобы сыграть в эту Раз*ёмную игру, ЗАРЕГИСТРИРУЙСЯ (АВТОРИЗУЙСЯ)
+        </h3>
+        <br />
+        <div className="mainBlock">
+          {themes.map((theme) => (
+            //  Передаем темы
+            <Themes key={theme.id} theme={theme} showButtons={false} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
